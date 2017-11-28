9642d1db77a5b413d5212c7fe6ea3335a7226a0d
Mipart <marcus.forsberg@falun.ntig.se>
2017-11-28 09:55:56 +0100

    Kalkylator(Form)
    + Kalkylator (form)

---
+++ Kalkylator(Forms)/Kalkylator(Forms)/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Kalkylator_Forms_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.cbFunc = new System.Windows.Forms.ComboBox();
+            this.txtFirstNum = new System.Windows.Forms.TextBox();
+            this.txtSecondNum = new System.Windows.Forms.TextBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "=";
+            // 
+            // txtSum
+            // 
+            this.txtSum.Location = new System.Drawing.Point(292, 10);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(100, 22);
+            this.txtSum.TabIndex = 1;
+            // 
+            // cbFunc
+            // 
+            this.cbFunc.FormattingEnabled = true;
+            this.cbFunc.Location = new System.Drawing.Point(112, 10);
+            this.cbFunc.Name = "cbFunc";
+            this.cbFunc.Size = new System.Drawing.Size(46, 24);
+            this.cbFunc.TabIndex = 2;
+            // 
+            // txtFirstNum
+            // 
+            this.txtFirstNum.Location = new System.Drawing.Point(6, 10);
+            this.txtFirstNum.Name = "txtFirstNum";
+            this.txtFirstNum.Size = new System.Drawing.Size(100, 22);
+            this.txtFirstNum.TabIndex = 3;
+            // 
+            // txtSecondNum
+            // 
+            this.txtSecondNum.Location = new System.Drawing.Point(164, 10);
+            this.txtSecondNum.Name = "txtSecondNum";
+            this.txtSecondNum.Size = new System.Drawing.Size(100, 22);
+            this.txtSecondNum.TabIndex = 4;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(82, 76);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(267, 69);
+            this.btnFirst.TabIndex = 5;
+            this.btnFirst.Text = "Preform Magic MATH!!!";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 161);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.txtSecondNum);
+            this.Controls.Add(this.txtFirstNum);
+            this.Controls.Add(this.cbFunc);
+            this.Controls.Add(this.txtSum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.ComboBox cbFunc;
+        private System.Windows.Forms.TextBox txtFirstNum;
+        private System.Windows.Forms.TextBox txtSecondNum;
+        private System.Windows.Forms.Button btnFirst;
+    }
+}
+
---
+++ Kalkylator(Forms)/Kalkylator(Forms)/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Kalkylator_Forms_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AddListItem();
+            cbFunc.SelectedIndex = 0;
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            PreformMagicMath();
+        }
+        
+        //Check if an valid input and choose Func
+        private void PreformMagicMath()
+        {
+            float numOne;
+            float numTwo;
+
+            bool returnOne = float.TryParse(txtFirstNum.Text, out numOne);
+            bool returnTwo = float.TryParse(txtSecondNum.Text, out numTwo);
+
+            //If both values can be parsed do....
+            if(returnOne && returnTwo){
+                switch (cbFunc.SelectedIndex)
+                {
+                    case 0:
+                        txtSum.Text = Add(numOne, numTwo).ToString();
+                        break;
+                    case 1:
+                        txtSum.Text = Sub(numOne, numTwo).ToString();
+                        break;
+                    case 2:
+                        txtSum.Text = Mult(numOne, numTwo).ToString();
+                        break;
+                    case 3:
+                        txtSum.Text = Div(numOne, numTwo).ToString();
+                        break;
+                }
+            }
+        }
+
+        //Add objects to List
+        private void AddListItem()
+        {
+            cbFunc.Items.Add("+");
+            cbFunc.Items.Add("-");
+            cbFunc.Items.Add("*");
+            cbFunc.Items.Add("/");
+
+        }
+
+        //Addition
+        private float Add(float firstNum, float secNum)
+        {
+            return firstNum + secNum;
+        }
+
+        //Multiplikation
+        private float Mult(float firstNum, float secNum)
+        {
+            return firstNum * secNum;
+        }
+
+        //Subtraktion
+        private float Sub(float firstNum, float secNum)
+        {
+            return firstNum - secNum;
+        }
+
+        //Division
+        private float Div(float firstNum, float secNum)
+        {
+            return firstNum / secNum;
+        }
+    }
+}
